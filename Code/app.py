dcceb726fe4ff6e60b9c169664e8bdd48db4d625
kismat91 <99793019+kismat91@users.noreply.github.com>
2023-12-03 17:23:28 -0500

    Streamlit app for DCGAN MODEl

--- Code/app.py
+++ Code/app.py
@@ -17,6 +17,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision.utils import make_grid
 from PIL import Image
+<<<<<<< HEAD
+from torchvision.transforms import ToPILImage
+import numpy as np
+=======
+>>>>>>> 5b48a10f22b65abc7d57674e639e4fb9b5340835
 import torchvision.transforms as transforms
 from train_DCGAN import Generator, Discriminator, weights_init  # Ensure this is correctly imported from your script
 from stanford_dogs_data import DogImages  # Ensure this is correctly imported from your script# Ensure this is correctly imported from your script
@@ -34,121 +39,69 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 
-# Establish convention for real and fake labels during training
-real_label = 1
-fake_label = 0
-
-def train_dcgan(epochs, batch_size, lr, beta1, DATA_DIR, MODEL_DIR):
-    # Initialize models
+# Function to load a model
+def load_model(model_path):
     netG = Generator(ngpu).to(device)
-    netD = Discriminator(ngpu).to(device)
+    netG.load_state_dict(torch.load(model_path))
+    netG.eval()
+    return netG
 
-    # Apply weight initialization or load pretrained model
-    netG.apply(weights_init)
-    netD.apply(weights_init)
-
-    # Initialize BCELoss function
-    criterion = nn.BCELoss()
-
-    # Setup Adam optimizers for both G and D
-    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
-
-    # Create the dataset and dataloader
-    transform = transforms.Compose([
-        transforms.Resize(64),
-        transforms.CenterCrop(64),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-    dataset = DogImages(root=DATA_DIR, train=True, download=False, cropped=True, transform=transform)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=WORKERS)
-
-
-    # Training Loop
-    for epoch in range(epochs):
-        for i, data in enumerate(dataloader, 0):
-            ############################
-            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-            ###########################
-            ## Train with all-real batch
-            netD.zero_grad()
-            # Format batch
-            real_data = data[0].to(device)
-            b_size = real_data.size(0)
-            label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-            # Forward pass real batch through D
-            output = netD(real_data).view(-1)
-            # Calculate loss on all-real batch
-            errD_real = criterion(output, label)
-            # Calculate gradients for D in backward pass
-            errD_real.backward()
-            D_x = output.mean().item()
-
-            ## Train with all-fake batch
-            # Generate batch of latent vectors
-            noise = torch.randn(b_size, NZ, 1, 1, device=device)
-            # Generate fake image batch with G
-            fake = netG(noise)
-            label.fill_(fake_label)
-            # Classify all fake batch with D
-            output = netD(fake.detach()).view(-1)
-            # Calculate D's loss on the all-fake batch
-            errD_fake = criterion(output, label)
-            # Calculate the gradients for this batch
-            errD_fake.backward()
-            D_G_z1 = output.mean().item()
-            # Add the gradients from the all-real and all-fake batches
-            errD = errD_real + errD_fake
-            # Update D
-            optimizerD.step()
-
-            ############################
-            # (2) Update G network: maximize log(D(G(z)))
-            ###########################
-            netG.zero_grad()
-            label.fill_(real_label) # fake labels are real for generator cost
-            # Since we just updated D, perform another forward pass of all-fake batch through D
-            output = netD(fake).view(-1)
-            # Calculate G's loss based on this output
-            errG = criterion(output, label)
-            # Calculate gradients for G
-            errG.backward()
-            D_G_z2 = output.mean().item()
-            # Update G
-            optimizerG.step()
-
-            # Output training stats to Streamlit (or console)
-            if i % 50 == 0:
-                st.write(f'[{epoch}/{epochs}][{i}/{len(dataloader)}]\tLoss_D: {errD.item():.4f}\tLoss_G: {errG.item():.4f}\tD(x): {D_x:.4f}\tD(G(z)): {D_G_z1:.4f} / {D_G_z2:.4f}')
-
-    # Save the model at the end of training
-    torch.save(netG.state_dict(), os.path.join(MODEL_DIR, 'DCGAN_netG_final.pth'))
-    torch.save(netD.state_dict(), os.path.join(MODEL_DIR, 'DCGAN_netD_final.pth'))
-    st.success('Training completed and models saved.')
-
-
-
+# Function to generate images
+def generate_images(netG, NZ, device):
+    # Generate a batch of latent vectors (random noise)
+    latent_vector = torch.randn(1, NZ, 1, 1, device=device)
+    # Generate fake image batch with G
+    fake_image = netG(latent_vector)
+    return fake_image
 
 # Streamlit Interface
 st.title('DCGAN for Stanford Dogs Dataset')
 
-# Sidebar for parameter settings
-st.sidebar.header('Training Parameters')
-epochs = st.sidebar.number_input('Epochs', min_value=1, value=30)
-batch_size = st.sidebar.number_input('Batch Size', min_value=1, value=64)
-lr = st.sidebar.number_input('Learning Rate', min_value=0.00001, max_value=0.1, value=0.0002, format="%.5f")
-beta1 = st.sidebar.slider('Beta1 for Adam Optimizer', min_value=0.0, max_value=1.0, value=0.5)
-
-DATA_DIR = st.sidebar.text_input('Data Directory', "/home/ubuntu/Deep-Learning/Data/")
+# Sidebar for model directory
 MODEL_DIR = st.sidebar.text_input('Model Directory', "/home/ubuntu/Deep-Learning/Models/")
 
-# Button to start training
-if st.sidebar.button('Start Training'):
-    with st.spinner('Training in progress...'):
-        train_dcgan(epochs, batch_size, lr, beta1, DATA_DIR, MODEL_DIR)
-        st.success('Training completed!')
+# Dropdown menu for pre-selected images (optional, for display purposes)
+# Dropdown menu for pre-selected images
+st.sidebar.header('Select an Input Image')
+image_options = {
+    "Chihuahua": "/home/ubuntu/Deep-Learning/Data/StanfordDogs/Images/n02085620-Chihuahua/n02085620_10074.jpg",
+    "Japanese Spaniel": "/home/ubuntu/Deep-Learning/Data/StanfordDogs/Images/n02085782-Japanese_spaniel/n02085782_1058.jpg",
+    "Maltese Dog": "/home/ubuntu/Deep-Learning/Data/StanfordDogs/Images/n02085936-Maltese_dog/n02085936_10297.jpg",
+    "African Hunting Dog": "/home/ubuntu/Deep-Learning/Data/StanfordDogs/Images/n02116738-African_hunting_dog/n02116738_10476.jpg"
+}
+selected_image_name = st.sidebar.selectbox('Select an Input Image', list(image_options.keys()))
+selected_image_path = image_options[selected_image_name]
 
+<<<<<<< HEAD
+# Display the selected image (optional)
+input_image = Image.open(selected_image_path)
+st.image(input_image, caption='Selected Input Image', use_column_width=True)
+
+# Load model
+model_files = os.listdir(MODEL_DIR)
+selected_model = st.sidebar.selectbox('Select a Model', model_files)
+model_path = os.path.join(MODEL_DIR, selected_model)
+netG = load_model(model_path)
+
+# Button to generate images
+if st.sidebar.button('Generate Image'):
+    with st.spinner('Generating Image...'):
+        output_tensor = generate_images(netG, NZ, device).squeeze(0).detach().cpu()
+        
+        # Normalize to [0, 1], then convert to [0, 255] and change to uint8
+        output_tensor = (output_tensor + 1) / 2  # Normalize to [0, 1]
+        output_tensor = output_tensor.clamp(0, 1)
+        output_tensor = output_tensor.numpy()
+        output_tensor = np.transpose(output_tensor, (1, 2, 0))  # Convert from CHW to HWC format
+        output_tensor = (output_tensor * 255).astype(np.uint8)  # Scale to [0, 255] and convert to uint8
+
+        output_image = ToPILImage()(output_tensor)
+
+        st.image(output_image, caption='Generated Image', use_column_width=True)
+
+        
+=======
+>>>>>>> 5b48a10f22b65abc7d57674e639e4fb9b5340835
 
 # Function to load and display generated images
 def display_generated_images(model_dir, ngpu, nz, device):
@@ -158,7 +111,11 @@ def display_generated_images(model_dir, ngpu, nz, device):
     netG.eval()  # Set the model to evaluation mode
 
     # Generate a batch of images
+<<<<<<< HEAD
+    fixed_noise = torch.randn(64, NZ , 1, 1, device=device)  # 64 images
+=======
     fixed_noise = torch.randn(64, nz, 1, 1, device=device)  # 64 images
+>>>>>>> 5b48a10f22b65abc7d57674e639e4fb9b5340835
     with torch.no_grad():
         fake_images = netG(fixed_noise).detach().cpu()
 
@@ -192,6 +149,10 @@ st.header("Real vs Fake Images")
 real_vs_fake_path = os.path.join(MODEL_DIR, 'DCGAN_real_vs_fake.png')
 if os.path.exists(real_vs_fake_path):
     real_vs_fake_images = Image.open(real_vs_fake_path)
+<<<<<<< HEAD
+    st.image(real_vs_fake_images, caption='Real vs Fake Images')
+=======
     st.image(real_vs_fake_images, caption='Real vs Fake Images')
     
 # Add additional Streamlit components as needed to display results, images, etc.
+>>>>>>> 5b48a10f22b65abc7d57674e639e4fb9b5340835
